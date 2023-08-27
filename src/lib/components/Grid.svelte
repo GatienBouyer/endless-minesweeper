43b37ce021dc2815af8d991fe755baf470f0b5d4
Gatien BOUYER <gatien.bouyer@ecole.ensicaen.fr>
2023-08-27 20:31:55 +0200

    Modify grid-go-up/left buttons to stick on the screen

--- src/lib/components/Grid.svelte
+++ src/lib/components/Grid.svelte
@@ -28,37 +28,48 @@
 		game.start();
 	});
 
+	let goUpButton: HTMLButtonElement;
 	function goUp() {
 		const value = parseInt(gridDiv.style.top);
 		const GO_UP_INCREMENT = 60;
 		const newValue = value + GO_UP_INCREMENT;
 		gridDiv.style.top = `${newValue}%`;
 	}
+	function stickUp() {
+		goUpButton.style.top = `-${window.scrollY}px`;
+	}
+
+	let goLeftButton: HTMLButtonElement;
 	function goLeft() {
 		const value = parseInt(gridDiv.style.left);
 		const GO_LEFT_INCREMENT = 60;
 		const newValue = value + GO_LEFT_INCREMENT;
 		gridDiv.style.left = `${newValue}%`;
 	}
+	function stickLeft() {
+		goLeftButton.style.left = `-${window.scrollX}px`;
+	}
 	// TODO add a (scrolling) margin around the cells
-	// TODO fixed position (on 1 axis) for buttons
 </script>
 
-<button on:click={goUp} id="goUp">Go up</button>
-<button on:click={goLeft} id="goLeft">Go left</button>
+<svelte:window on:scroll={stickUp} on:scroll={stickLeft} />
+<button bind:this={goUpButton} on:click={goUp} id="goUp">Go up</button>
+<button bind:this={goLeftButton} on:click={goLeft} id="goLeft">Go left</button>
 <div bind:this={gridDiv} style="top: 50%; left: 50%;" />
 
 <style>
 	#goUp {
-		position: absolute;
+		position: fixed;
 		left: 50%;
 		translate: -50%;
+		margin-top: 1em;
 		z-index: 99;
 	}
 	#goLeft {
-		position: absolute;
+		position: fixed;
 		top: 50%;
 		translate: 0 -50%;
+		margin-left: 1em;
 		z-index: 99;
 	}
 	div {
